b3e126bc545287061564d708d12df1dd5b05b66f
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2024-10-04 23:23:17 +0300

    updates to getTopSearches
    function now sorts returned array and returns first 5 searches

--- controllers/getTopSearches.js
+++ controllers/getTopSearches.js
@@ -8,6 +8,12 @@ async function getTopSearches() {
 
   const response = await searchModel.find();
 
+  const sortedResponse = response.sort(
+    (a, b) => b.searchFrequency - a.searchFrequency
+  );
+
+  console.log(sortedResponse.slice(0, 5));
+
   return response;
 }
 module.exports = getTopSearches;
