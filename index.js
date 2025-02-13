0016bb72bd581624e6a1ec7a936274cd6510ea11
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2025-02-13 09:38:03 +0200

    Trying new service

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3000;
 
 app.options('*', cors()); // Handling pre-flight OPTIONS request globally
 
