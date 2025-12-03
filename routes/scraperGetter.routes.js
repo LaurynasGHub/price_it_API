7d8a5a0bc9dd81977934b46826fa1677a23e4458
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2025-12-03 22:51:53 +0200

    BUG FIXES

--- routes/scraperGetter.routes.js
+++ routes/scraperGetter.routes.js
@@ -18,8 +18,6 @@ router.get('/shops/results', async (req, res) => {
   try {
     const searchTerm = req.query.searchTerm;
 
-    console.log('ALLOWED_ORIGINS:', process.env.ALLOWED_ORIGINS);
-
     if (!allowedOrigins.includes(req.headers.origin?.trim())) {
       console.log(
         ' >>> Blocked request to /shops/results.\n > Origin - ',
