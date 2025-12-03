10553090f7bf9f9bea0bd9e5f3657f65d05ab035
Laurynas Gailius <150287011+LaurynasGHub@users.noreply.github.com>
2025-12-03 21:14:58 +0200

    A lot of updates on scrapers
    Improvements to scraper - removed unused and unneccessary code, removed comments, updates to general logic and removed unnecessary calls etc.

--- routes/scraperGetter.routes.js
+++ routes/scraperGetter.routes.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
-const express = require('express');
-const cors = require('cors');
-const app = express();
+
+const allowedOrigins = require('../allowedOrigins.json');
 
 const {
   getBarboraScraperResults,
@@ -15,19 +14,9 @@ const router = Router();
 // Get shops scraper results
 router.get('/shops/results', async (req, res) => {
   try {
-    // extract the search term from the query parameters
     const searchTerm = req.query.searchTerm;
 
-    // allowed origins
-    const allowedOrigins = [
-      'https://price-it.vercel.app',
-      'http://localhost:3001',
-    ];
-
-    const origin = req.headers.origin;
-    console.log('Origin:', origin);
-
-    if (!allowedOrigins.includes(origin)) {
+    if (!allowedOrigins.includes(req.headers.origin)) {
       return res.status(520).json({ error: 'Unknown error' });
     }
 
@@ -36,28 +25,17 @@ router.get('/shops/results', async (req, res) => {
     }
 
     let returnJson = {};
-    // Barbora scraper
-    const barboraData = await getBarboraScraperResults(searchTerm);
-    // Rimi scraper
-    const rimiData = await getRimiScraperResults(searchTerm);
-    // Last Mile scraper
-    const lastMileData = await getLastMileScraperResults(searchTerm);
+    const [barboraData, rimiData, lastMileData] = await Promise.all([
+      getBarboraScraperResults(searchTerm),
+      getRimiScraperResults(searchTerm),
+      getLastMileScraperResults(searchTerm),
+    ]);
 
     returnJson.barbora = barboraData;
-
     returnJson.rimi = rimiData;
-
     returnJson.lastMile = lastMileData;
 
     res.json(returnJson);
-    // check if json is not empty, if not-update search term frequency in the database
-    if (
-      returnJson.barbora.length > 0 ||
-      returnJson.rimi.length > 0 ||
-      returnJson.lastMile.length > 0
-    ) {
-      addSearch(req.query.searchTerm);
-    }
   } catch (error) {
     console.log('Error:', error.message);
     res.status(400).json({ error: error.message });
@@ -67,17 +45,18 @@ router.get('/shops/results', async (req, res) => {
 // Get alcohol shops scraper results
 router.get('/shops/alcohol/results', async (req, res) => {
   try {
-    // extract the search term from the query parameters
     const searchTerm = req.query.searchTerm;
+
+    if (!allowedOrigins.includes(req.headers.origin)) {
+      return res.status(520).json({ error: 'Unknown error' });
+    }
+
     if (!searchTerm) {
       return res.status(400).json({ error: 'Search term is required' });
     }
-    // update search term frequency in the database
-    // Alcohol shops diff db?
-    // addSearch(req.query.searchTerm);
 
     let returnJson = {};
-    // Vynoteka scraper
+
     const vynotekaData = await getVynotekaScraperResults(searchTerm);
 
     returnJson.vynoteka = vynotekaData;
